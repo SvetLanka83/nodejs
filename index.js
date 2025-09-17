3aeba2d38149c68204c0073cb43ab8cef74d375c
SvetLanka83 <svetulichka@gmail.com>
2025-09-17 21:02:16 +0200

    init project

--- index.js
+++ index.js
@@ -1,12 +1,47 @@
-const http = require('node:http');
-const foo = async ()=> {// Create a local server to receive data from
-   const server = http.createServer((req, res) => {
-      res.writeHead(200, {'Content-Type': 'application/json'});
-      res.end(JSON.stringify({
-         data: 'Hello World!',
-      }));
-   });
+const express = require("express");
+const {use} = require("express/lib/application");
 
-   server.listen(3000);
-}
-void foo();
+ const app = express();
+ // app.use(express.json());
+ // app.use(express.urlencoded({ extended: true }));
+
+ app.get("/users", (req, res) => {
+     res.send('Hello World!');
+ })
+
+app.post("/users", (req, res) => {
+   res.send('Hello World!2');
+})
+
+app.listen(3000, () => {
+   console.log("Server running on http://localhost:3000/");
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+   // const server = http.createServer((req, res) => {
+   //    if (req.url === '/users' && req.method == 'GET') {
+   //       res.writeHead(200, {'Content-Type': 'application/json'});
+   //       res.end(JSON.stringify({
+   //          data: 'Hello World!',
+   //       }));
+   //       return
+   //    }
+   //    res.writeHead(200, {'Content-Type': 'application/json'});
+   //    res.end(JSON.stringify({
+   //       data: 'Hello World!',
+   //    }));
+   // });
+   //
+   // server.listen(3000);
+
+
