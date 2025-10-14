b232c5eeec298049ef302efe40b73f71f9bc910a
SvetLanka83 <svetulichka@gmail.com>
2025-10-14 20:25:18 +0200

    init project

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -4,6 +4,8 @@ import { IUser } from "../interfaces/user.interface";
 
 const userSchema = new Schema(
     {
+        email: { type: String, required: true, unique: true },
+        password: { type: String, required: true },
         name: { type: String, required: true },
         surname: { type: String, required: true },
         age: { type: Number, required: true },
